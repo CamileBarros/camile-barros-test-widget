2a388b4f970ddfb1d704cd8826b906fd96f93ce0
camilebarroswarren <camile.barros@warren.com.br>
2022-07-08 16:36:01 -0300

    feat: new fab

--- lib/component/HelperText.dart
+++ lib/component/HelperText.dart
@@ -6,15 +6,27 @@ class HelperText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-        height: 44,
-        width: 125,
-        child: TextButton(
-          style: TextButton.styleFrom(
-              primary: const Color(0xFFF5F6FA),
-              backgroundColor: const Color(0xFFE02B57)),
-          onPressed: () {},
-          child: const Text('Clique'),
-        ));
+    return Column(
+      children: [
+        SizedBox(
+          height: 44,
+          width: 125,
+          child: TextButton(
+            style: TextButton.styleFrom(
+                primary: const Color(0xFFF5F6FA),
+                backgroundColor: const Color(0xFFE02B57)),
+            onPressed: () {},
+            child: const Text('Clique'),
+          ),
+        ),
+        const SizedBox(
+          height: 44,
+          width: 125,
+          child: FloatingActionButton(
+            onPressed: null,
+          ),
+        )
+      ],
+    );
   }
 }
