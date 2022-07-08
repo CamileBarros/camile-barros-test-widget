e944f7ca8cf6c26cbd46cd9de59739b5e58ba18f
camilebarroswarren <camile.barros@warren.com.br>
2022-07-08 12:12:03 -0300

    feat:TextButton to test

--- lib/component/HelperText.dart
+++ lib/component/HelperText.dart
@@ -9,7 +9,16 @@ class HelperText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text(text),
-    );
+        child: SizedBox(
+            height: 44,
+            width: 125,
+            child: TextButton.icon(
+              style: TextButton.styleFrom(
+                  primary: const Color(0xFFF5F6FA),
+                  backgroundColor: const Color(0xFFE02B57)),
+              label: const Text('Clique'),
+              icon: const Icon(Icons.ads_click),
+              onPressed: () {},
+            )));
   }
 }
